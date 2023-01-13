3949e4982420ca13839c7787d0029976e6f91e30
arka1002 <arka.mcu700@gmail.com>
2023-01-13 14:07:24 +0530

    css change

--- src/routes/video.jsx
+++ src/routes/video.jsx
@@ -1,70 +1,49 @@
 import { API, Amplify, graphqlOperation } from 'aws-amplify';
 import { useLoaderData } from "react-router-dom";
-import { getTodo, listTodos } from "../graphql/queries";
+import { listTodos } from "../graphql/queries";
 import { useQuery } from "@tanstack/react-query";
 import { Heading, Text } from "@aws-amplify/ui-react";
 import awsExports from '../aws-exports';
 import '@aws-amplify/ui-react/styles.css';
 import Iframe from 'react-iframe'
 
+
 Amplify.configure(awsExports);
-
-
-
-
 
 
 export default function Video() {
     const videos = useLoaderData();
-
-
-
     return (
         <div id="video">
-            <Heading
-                width='30vw'
-                level={6}
-            >
-                {videos.name}
-            </Heading>
 
-            <Text
-                variation="primary"
-                as="p"
-                color="blue"
-                lineHeight="1.5em"
-                fontWeight={400}
-                fontSize="1em"
-                fontStyle="normal"
-                textDecoration="none"
-                width="30vw"
-            >
-                {videos.description}
-            </Text>
+
+            <div className='mt-10 flex flex-row justify-evenly items-center'>
+                <div>
+                    <span className='text-xl italic font-bold underline underline-offset-2'>
+                        {videos.name}
+                    </span>
+
+                    <p className='w-96'>
+                        {videos.description}
+                    </p>
+                </div>
+                <div>
+                    <Iframe url={videos.contentLink}
+                        width="640px"
+                        height="320px"
+                        id=""
+                        className=""
+                        display="block"
+                        position="relative" />
+                </div>
+            </div>
+
+
+
 
             <ReccedVideos category={videos.category} />
 
-            <Text
-                variation="primary"
-                as="p"
-                color="black"
-                lineHeight="1.5em"
-                fontWeight={400}
-                fontSize="1em"
-                fontStyle="normal"
-                textDecoration="none"
-                width="30vw"
-            >
-                Category :- {videos.category}
-            </Text>
 
-            <Iframe url={videos.contentLink}
-                width="640px"
-                height="320px"
-                id=""
-                className=""
-                display="block"
-                position="relative" />
         </div>
     );
 };
