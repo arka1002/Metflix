309809cccbcaeee81c23c89425bd72396e2fa8cc
arka1002 <arka.mcu700@gmail.com>
2023-01-15 14:41:57 +0530

    made my 1st protected route bruh!!!!!!!!!!

--- src/routes/myprofile.jsx
+++ src/routes/myprofile.jsx
@@ -7,7 +7,8 @@ import {
     useMutation,
     useQueryClient,
 } from '@tanstack/react-query';
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation, Navigate } from "react-router-dom";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
 
 
@@ -19,6 +20,23 @@ Amplify.configure(awsExports);
 export default function Myprofile() {
 
 
+    const location = useLocation();
+    const { route } = useAuthenticator((context) => [context.route]);
+
+
+    const { user, signOut } = useAuthenticator((context) => [context.user]);
+
+    if (route !== 'authenticated') {
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+    return <MainList user = {user}/>;
+
+};
+
+
+
+function MainList({ user }) {
     // Access the client
     const queryClient = useQueryClient();
 
@@ -46,24 +64,25 @@ export default function Myprofile() {
 
     return (
         <>
-        <p className="text-xl italic font-bold underline underline-offset-2 text-center mt-5">My liked Videos :-</p>
-        <ul class="list-disc text-center">
-        {
-            likedVideosList.filter(video => video.isLiked == "yes").map(video => (
-                
-                <NavLink to={`/videos/${video.id}`}><li className='text-amber-600 italic text-center'>{video.name}</li></NavLink>
-            ))
-        }
-        </ul>
-        <p className="text-xl italic font-bold underline underline-offset-2 text-center mt-5">My playlist :-</p>
-        <ul class="list-disc text-center">
-        {
-            likedVideosList.filter(video => video.isWatchedLater == "yes").map(video => (
-                
-                <NavLink to={`/videos/${video.id}`}><li className='text-amber-600 italic text-center'>{video.name}</li></NavLink>
-            ))
-        }
-        </ul>
+            <p>{user.username}</p>
+            <p className="text-xl italic font-bold underline underline-offset-2 text-center mt-5">My liked Videos :-</p>
+            <ul class="list-disc text-center">
+                {
+                    likedVideosList.filter(video => video.isLiked == "yes").map(video => (
+
+                        <NavLink to={`/videos/${video.id}`}><li className='text-amber-600 italic text-center'>{video.name}</li></NavLink>
+                    ))
+                }
+            </ul>
+            <p className="text-xl italic font-bold underline underline-offset-2 text-center mt-5">My playlist :-</p>
+            <ul class="list-disc text-center">
+                {
+                    likedVideosList.filter(video => video.isWatchedLater == "yes").map(video => (
+
+                        <NavLink to={`/videos/${video.id}`}><li className='text-amber-600 italic text-center'>{video.name}</li></NavLink>
+                    ))
+                }
+            </ul>
         </>
     );
-};
+}
