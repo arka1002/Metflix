68effe8c489fb9de965d62a94d0ba12b4f29690d
arka1002 <arka.mcu700@gmail.com>
2023-01-15 11:14:08 +0530

    added auth successfully

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -1,20 +1,29 @@
 import { Authenticator, useAuthenticator, View } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import { redirect } from "react-router-dom";
+import { redirect, useNavigate, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 
 export default function Login() {
     const { route } = useAuthenticator((context) => [context.route]);
+    const location = useLocation();
+    const navigate = useNavigate();
+    let from = location.state?.from?.pathname || '/';
+    useEffect(() => {
+        if (route === 'authenticated') {
+            navigate(from, { replace: true });
+        }
+    }, [route, navigate, from]);
 
 
-    if (route === 'authenticated') {
-        return redirect(`/`);
-    }
+
+
+
 
     return (
-        <View>
+        <div className='flex justify-center mt-10'>
             <Authenticator></Authenticator>
-        </View>
+        </div>
 
     );
 
